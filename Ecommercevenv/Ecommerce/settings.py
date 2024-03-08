2bef36cb23732ee6594b01de6615348d533dffbd
jenish-ml <jenishpunjiri@gmail.com>
2024-03-09 01:56:01 +0530

    host

--- Ecommercevenv/Ecommerce/settings.py
+++ Ecommercevenv/Ecommerce/settings.py
@@ -59,7 +59,7 @@ ROOT_URLCONF = 'Ecommerce.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -121,9 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR/ "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
+STATIC_ROOT = os.path.join(BASE_DIR,'assets')
 MEDIA_URL = 'uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR,'uploads/')
+MEDIA_ROOT = os.path.join(BASE_DIR ,'uploads/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
